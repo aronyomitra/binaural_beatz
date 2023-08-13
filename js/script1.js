ed0c889c695618f6ac52a07f9884547fe501544c
Aronyo Mitra <aronyomitra@gmail.com>
2023-08-13 23:49:14 +0530

    Add basic structure to code and sample beat

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,58 @@
+const globalDefaults = {
+    frequencyLeft: 150,
+    frequencyRight: 156,
+    panLeft: -1,
+    panRight: 1,
+    waveType: "sine"
+}
+
+let globalAudioVars = {
+    audioContext: null,
+    oscLeft: null,
+    oscRight: null,
+    pannerNodeLeft: null,
+    pannerNodeRight: null,
+    gainNodeLeft: null,
+    gainNodeRight: null
+}
+
 window.onload = function() {
     console.log("Loaded page");
 
+    // setupAudio();
+
+}
+
+function setupAudio() {
+
+    const audioContextClass = window.AudioContext || window.webkitAudioContext; 
+    globalAudioVars.audioContext = new audioContextClass();
+
+    globalAudioVars.oscLeft = globalAudioVars.audioContext.createOscillator();
+    globalAudioVars.oscLeft.type = globalDefaults.waveType;
+    globalAudioVars.oscLeft.frequency.value = globalDefaults.frequencyLeft;
+
+    globalAudioVars.pannerNodeLeft = globalAudioVars.audioContext.createStereoPanner();
+    globalAudioVars.pannerNodeLeft.pan.value = globalDefaults.panLeft;
+    
+    globalAudioVars.oscLeft.connect(globalAudioVars.pannerNodeLeft).connect(globalAudioVars.audioContext.destination);
+
+    globalAudioVars.oscRight = globalAudioVars.audioContext.createOscillator();
+    globalAudioVars.oscLeft.type = globalDefaults.waveType;
+    globalAudioVars.oscRight.frequency.value = globalDefaults.frequencyRight;
+
+    globalAudioVars.pannerNodeRight = globalAudioVars.audioContext.createStereoPanner();
+    globalAudioVars.pannerNodeRight.pan.value = globalDefaults.panRight;
+
+    globalAudioVars.oscRight.connect(globalAudioVars.pannerNodeRight).connect(globalAudioVars.audioContext.destination);
 }
 
 function generateAudio() {
-    const audioContextClass = window.AudioContext || window.webkitAudioContext; 
-    const audioContext = new audioContextClass();
 
-    var osc = audioContext.createOscillator();
+    setupAudio();
+    globalAudioVars.oscLeft.start();
+    globalAudioVars.oscLeft.stop(globalAudioVars.audioContext.currentTime + 3);
 
-    osc.type = "sine";
-    osc.frequency.value = 150;
-    osc.connect(audioContext.destination);
-    osc.start();
-
-    osc.stop(5);
+    globalAudioVars.oscRight.start();
+    globalAudioVars.oscRight.stop(globalAudioVars.audioContext.currentTime + 3);
 }
